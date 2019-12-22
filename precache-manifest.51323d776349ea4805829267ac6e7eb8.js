de729b7211dfc8dfac2414823278c48341d23e7f
henry-49 <henrynwadiogor@gmail.com>
2019-12-22 19:23:35 +0800

    Updates

---
+++ precache-manifest.51323d776349ea4805829267ac6e7eb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaebe9baff93d02b74d6c1c55d3178cc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f439a95d8850a87d691a",
+    "url": "/monsters-rolodex/static/css/main.bb37eb98.chunk.css"
+  },
+  {
+    "revision": "1c8ab6b652225693b469",
+    "url": "/monsters-rolodex/static/js/2.015ee11f.chunk.js"
+  },
+  {
+    "revision": "f439a95d8850a87d691a",
+    "url": "/monsters-rolodex/static/js/main.b68fcba1.chunk.js"
+  },
+  {
+    "revision": "5b3963d0ec88b4c7198b",
+    "url": "/monsters-rolodex/static/js/runtime-main.08369cb2.js"
+  }
+]);
